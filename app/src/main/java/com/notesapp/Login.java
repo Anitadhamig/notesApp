c7bdb8765ba71d553c9d47494a8bef382640991b
anitadhami <anita.sportena@gmail.com>
2021-04-21 13:21:08 +0530

    first commit

---
+++ app/src/main/java/com/notesapp/Login.java
@@ -0,0 +1,16 @@
+
+
+package com.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
